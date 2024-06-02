89df844a0b214fcd88477d612f5d1819926387d8
Sebastian Francisco Belmonte Cerveró <sbelmonte@servicios.mc4.com.bo>
2024-06-02 12:32:17 -0400

    GES-184 FIX on text

--- lib/data/provider/api.dart
+++ lib/data/provider/api.dart
@@ -1,4 +1,4 @@
 class Api {
+ //static const String url = "http://localhost:8080/api/v1";
   static const String url = "http://localhost:8080/api/v1";
-  //static const String url = "http://172.18.4.182:8080/api/v1";
 }
