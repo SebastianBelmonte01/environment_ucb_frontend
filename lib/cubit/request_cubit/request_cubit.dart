4b0bb9abe597402533d8b8c9fd6ee354e7ec49a0
IsaiDiaz <jsoza057@gmail.com>
2024-06-02 14:01:23 -0400

    GES-182 Fixed time selection

--- lib/cubit/request_cubit/request_cubit.dart
+++ lib/cubit/request_cubit/request_cubit.dart
@@ -4,7 +4,6 @@ import 'package:equatable/equatable.dart';
 import 'package:http/http.dart';
 import 'package:intl/intl.dart';
 
-
 import '../../services/request_service.dart';
 import '../page_status.dart';
 
@@ -13,7 +12,8 @@ part 'request_state.dart';
 class RequestCubit extends Cubit<RequestState> {
   RequestCubit() : super(RequestState());
 
-  Future<void> postRequest(String environment, String subject, int parallel, int people, String reason) async {
+  Future<void> postRequest(String environment, String subject, int parallel,
+      int people, String reason) async {
     emit(state.copyWith(status: PageStatus.loading));
     try {
       RequestDto newRequest = RequestDto(
@@ -36,19 +36,42 @@ class RequestCubit extends Cubit<RequestState> {
     }
   }
 
-  
-
   void setDate(DateTime date) {
     emit(state.copyWith(date: date));
   }
+
   void setInitTime(DateTime initTime) {
-    emit(state.copyWith(initTime: initTime));
-  }
-  void setEndTime(DateTime endTime) {
-    emit(state.copyWith(endTime: endTime));
+    if (state.endTime != null) {
+      if (isValidInitTime(initTime, state.endTime!)) {
+        emit(state.copyWith(initTime: initTime));
+      }
+    } else {
+      emit(state.copyWith(initTime: initTime));
+    }
   }
 
-  void setSelectedRequest(RequestDto requestDto){
+  void setEndTime(DateTime endTime) {
+    if (state.initTime != null) {
+      if (isValidEndTime(state.initTime!, endTime)) {
+        emit(state.copyWith(endTime: endTime));
+      }else{
+        emit(state.copyWith(endTime: null));
+      }
+    } else {
+      emit(state.copyWith(endTime: endTime));
+    }
+  }
+
+  bool isValidInitTime(DateTime initTime, DateTime endTime) {
+    return initTime.isBefore(endTime) &&
+        endTime.difference(initTime).inHours >= 1;
+  }
+
+  bool isValidEndTime(DateTime initTime, DateTime endTime) {
+    return endTime.isAfter(initTime) && endTime.difference(initTime).inHours >= 1;
+  }
+
+  void setSelectedRequest(RequestDto requestDto) {
     emit(state.copyWith(request: requestDto));
   }
 }
